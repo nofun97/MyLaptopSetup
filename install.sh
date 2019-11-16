029e0d6cbf9651e743455c7b01ca8b0269add69d
Novan Allanadi <novan.allanadi1211@gmail.com>
2019-11-16 17:33:40 +1100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
+# adapted from https://github.com/donnemartin/dev-setup/blob/master/brew.sh
 
 cd $HOME
 
 # Ask for the administrator password upfront
 sudo -v
 
-# Keep-alive: update existing `sudo` time stamp until `osx.sh` has finished
+# Keep-alive: update existing `sudo` time stamp until the script has finished.
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
 # brew-cask
