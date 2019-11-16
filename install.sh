9984eef88b31d092a8e23560aac55648ca40700d
Novan Allanadi <novan.allanadi1211@gmail.com>
2019-11-16 13:41:11 +1100

    added node installs

--- install.sh
+++ install.sh
@@ -33,3 +33,14 @@ brew install go
 git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
 git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+
+# vim
+brew install vim --with-override-system-vi
+
+# install node and stuff
+brew install nvm
+source $(brew --prefix nvm)/nvm.sh
+exec -l $SHELL
+NVM_DIR=$HOME/.nvm
+nvm install node
+brew install yarn
