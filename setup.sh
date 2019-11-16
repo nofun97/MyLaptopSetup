574947d42049461a7754d0ee73afe767740723a1
Novan Allanadi <novan.allanadi1211@gmail.com>
2019-11-16 12:26:18 +1100

    added setup to zsh and brew

---
+++ setup.sh
@@ -0,0 +1,23 @@
+# adapted from https://github.com/donnemartin/dev-setup/blob/master/brew.sh
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until `osx.sh` has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# install xcode CLI
+xcode-select --install
+
+# install brew
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Make sure we’re using the latest Homebrew.
+brew update
+
+# Upgrade any already-installed formulae.
+brew upgrade --all
+
+# zsh
+brew install zsh zsh-completions
+chsh -s /bin/zsh
