72eaa32f30155322df2718a094a2de1344187a92
Novan Allanadi <novan.allanadi1211@gmail.com>
2019-11-16 12:40:49 +1100

    add zsh plugins

--- install.sh
+++ install.sh
@@ -25,3 +25,8 @@ pip3 install --user virtualenv
 
 # golang
 brew install go
+
+# zsh plugins and theme
+git clone --depth=1 https://github.com/romkatv/powerlevel10k.git $ZSH_CUSTOM/themes/powerlevel10k
+git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
