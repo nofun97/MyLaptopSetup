2aeb39a4424f78ffed06bf2d27554044ce165e0f
Novan Allanadi <novan.allanadi1211@gmail.com>
2019-11-16 12:26:59 +1100

    removed setup scripts

--- install.sh
+++ install.sh
@@ -8,17 +8,6 @@ sudo -v
 # Keep-alive: update existing `sudo` time stamp until `osx.sh` has finished
 while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
 
-# install xcode CLI
-xcode-select --install
-
-# install brew
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
-brew doctor
-
-# zsh
-brew install zsh zsh-completions
-chsh -s /bin/zsh
-
 # install java 8
 brew tap homebrew/cask-versions
 brew cask install java8
